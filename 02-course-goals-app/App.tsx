6962b68a1515518f1f4f1b57b50ebb75630a9287
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-08-16 11:16:45 +0200

    added text input

--- 02-course-goals-app/App.tsx
+++ 02-course-goals-app/App.tsx
@@ -1,14 +1,29 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-	return (
-		<View className='flex-1 items-center justify-center bg-white'>
-			<Text className='text-red-500'>
-				Open up App.js to start working on your app!
-			</Text>
-			<StatusBar style='auto' />
-		</View>
-	);
+  const [text, setText] = useState('');
+
+  function onTextInputChange(text: string) {
+    setText(text);
+  }
+
+  return (
+    <View className='flex-1 items-center justify-between bg-white pb-10 pt-14'>
+      <Text className='mb-12'>{text}</Text>
+      <View className='flex w-[90vw] flex-row content-center items-center gap-2 rounded-md bg-gray-800 px-2 py-1'>
+        <TouchableOpacity className='-translate-y-[5.5px] px-2 py-1'>
+          <Text className='m-auto text-3xl font-semibold text-white'>+</Text>
+        </TouchableOpacity>
+        <TextInput
+          textAlign='left'
+          textAlignVertical='center'
+          className='h-full w-full -translate-y-[7px] py-2 text-lg font-semibold text-white'
+          placeholder='Enter Your Course Goals!'
+          placeholderTextColor={'lightgrey'}
+          onChangeText={onTextInputChange}
+        />
+      </View>
+    </View>
+  );
 }

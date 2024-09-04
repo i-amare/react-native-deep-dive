4b3ce2e8f1be26e58de843ea82389aeac4f59781
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-04 13:56:41 +0200

    increased size of text input to make clicking it easier

--- 04-number-guesser/app/components/textInput.tsx
+++ 04-number-guesser/app/components/textInput.tsx
@@ -6,9 +6,9 @@ export default function NumberInput() {
       <TextInput
         maxLength={2}
         keyboardType='number-pad'
-        className='h-24 translate-y-8 overflow-visible text-6xl font-semibold'
+        className='h-24 w-24 translate-y-8 overflow-visible text-center text-6xl font-semibold text-slate-800'
       />
-      <View className='h-1 w-24 rounded-md bg-black' />
+      <View className='h-1 w-24 rounded-md bg-slate-800' />
     </View>
   );
 }

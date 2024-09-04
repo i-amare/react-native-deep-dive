3394ac9eb3771369146a02108bd13f00b9ca204d
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-04 17:36:24 +0200

    number input + basic validation

--- 04-number-guesser/app/components/textInput.tsx
+++ 04-number-guesser/app/components/textInput.tsx
@@ -1,11 +1,22 @@
-import { TextInput, View } from 'react-native';
+import {
+  NativeSyntheticEvent,
+  TextInput,
+  TextInputKeyPressEventData,
+  View,
+} from 'react-native';
 
-export default function NumberInput() {
+type NumberInputProps = {
+  text: string;
+  onKeyPress: (e: NativeSyntheticEvent<TextInputKeyPressEventData>) => void;
+};
+
+export default function NumberInput({ text, onKeyPress }: NumberInputProps) {
   return (
     <View className='mb-4 flex w-full items-center justify-center'>
       <TextInput
-        maxLength={2}
         keyboardType='number-pad'
+        onKeyPress={onKeyPress}
+        value={text}
         className='h-24 w-24 translate-y-8 overflow-visible text-center text-6xl font-semibold text-slate-800'
       />
       <View className='h-1 w-24 rounded-md bg-slate-800' />
--- 04-number-guesser/app/index.tsx
+++ 04-number-guesser/app/index.tsx
@@ -1,9 +1,33 @@
 import { BlurView } from 'expo-blur';
-import { View } from 'react-native';
+import { useState } from 'react';
+import {
+  NativeSyntheticEvent,
+  TextInputKeyPressEventData,
+  View,
+} from 'react-native';
 import Button from './components/button';
 import NumberInput from './components/textInput';
 
 export default function HomePage() {
+  const [enteredNumber, setEnteredNumber] = useState('0');
+
+  function onKeyPress(e: NativeSyntheticEvent<TextInputKeyPressEventData>) {
+    const key = e.nativeEvent.key;
+
+    if (key == 'Backspace') {
+      setEnteredNumber((prev) => prev[prev.length - 2] || '0');
+      return;
+    }
+
+    const acceptedCharacters = '0123456789';
+    if (acceptedCharacters.indexOf(key) == -1) return;
+    if (enteredNumber.length >= 2) return;
+
+    setEnteredNumber((prev) => {
+      return Number(prev + key).toString();
+    });
+  }
+
   return (
     <View className='flex-1 items-center justify-center'>
       <View className='overflow-hidden rounded-xl bg-white/30'>
@@ -12,7 +36,7 @@ export default function HomePage() {
           tint='light'
           className='flex items-center justify-center rounded-md px-4 pb-8'
         >
-          <NumberInput />
+          <NumberInput text={enteredNumber} onKeyPress={onKeyPress} />
           <View className='flex flex-row justify-center space-x-4'>
             <Button text='Start' />
             <View className='w-4' />

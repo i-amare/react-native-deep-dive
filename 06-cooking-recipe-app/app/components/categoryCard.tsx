faf6cd6afa52ce1be010a3ebcf5431ce87c70c9f
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-08 09:41:00 +0200

    Create categoryCard.tsx

---
+++ 06-cooking-recipe-app/app/components/categoryCard.tsx
@@ -0,0 +1,10 @@
+import Category from "@/models/category";
+import { View } from "react-native";
+
+type CategoryCardProps = Category
+
+export default function CategoryCard({id, title, colour}: CategoryCardProps) {
+	return (
+		<View></View>
+	)
+}

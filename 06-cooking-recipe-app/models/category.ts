7305d719e0909ff1993f8185ee978b43419f2786
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-08 09:33:52 +0200

    added dummy data to app

---
+++ 06-cooking-recipe-app/models/category.ts
@@ -0,0 +1,13 @@
+class Category {
+  id: string;
+  title: string;
+  colour: string;
+
+  constructor(id: string, title: string, colour: string) {
+    this.id = id;
+    this.title = title;
+    this.colour = colour;
+  }
+}
+
+export default Category;

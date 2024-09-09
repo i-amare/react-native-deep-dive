85b499b2f0022e9478e578573c147136b7301aea
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-09 13:26:19 +0200

    styling changes

--- 06-cooking-recipe-app/app/components/categoryCard.tsx
+++ 06-cooking-recipe-app/app/components/categoryCard.tsx
@@ -9,7 +9,7 @@ export default function CategoryCard({
   imageUrl,
 }: CategoryCardProps) {
   return (
-    <View className='relative mx-2 mt-14 flex aspect-[4/5] flex-1 items-center justify-center rounded-3xl bg-gray-900 p-4'>
+    <View className='relative mx-2 mt-14 flex aspect-[3/3] flex-1 items-center justify-center rounded-3xl bg-gray-900 p-4'>
       <View className='absolute top-0 aspect-square w-28 -translate-y-6 overflow-hidden rounded-full bg-white'>
         <Image
           source={{
@@ -18,7 +18,7 @@ export default function CategoryCard({
           className='w-dull h-full'
         />
       </View>
-      <Text className='w-full text-center text-lg font-semibold text-white'>
+      <Text className='w-full translate-y-8 text-center text-xl font-semibold text-white'>
         {title}
       </Text>
     </View>

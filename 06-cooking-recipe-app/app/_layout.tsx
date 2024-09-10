7779a8c2e1af3af8192d5ae35e4e14050021a57e
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-10 17:16:16 +0200

    disable header

--- 06-cooking-recipe-app/app/_layout.tsx
+++ 06-cooking-recipe-app/app/_layout.tsx
@@ -1,12 +1,15 @@
-import { Stack } from 'expo-router';
-import { SafeAreaView } from 'react-native';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { Stack, useRouter } from 'expo-router';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 
 export default function RootLayout() {
   return (
     <SafeAreaView className='flex-1 bg-gray-950'>
-      <Stack screenOptions={{
-        headerBackground: () => null
-      }} />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </SafeAreaView>
   );
 }

b27cf3d574289f4439e6b3af11e5ad66f22a43ed
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-10 16:25:58 +0200

    Update index.tsx

--- 06-cooking-recipe-app/app/index.tsx
+++ 06-cooking-recipe-app/app/index.tsx
@@ -1,16 +1,17 @@
 import { FlatList, Text, View } from 'react-native';
 import { CATEGORIES as mealCatagories } from '@/data/mealData';
 import CategoryCard from './components/categoryCard';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function HomePage() {
   return (
     <View className='flex-1 items-center justify-center bg-gray-950'>
-        <FlatList
-          data={mealCatagories}
-          numColumns={2}
-          renderItem={({ item }) => <CategoryCard {...item} />}
-          className='w-full'
-        />
+      <FlatList
+        data={mealCatagories}
+        numColumns={2}
+        renderItem={({ item }) => <CategoryCard {...item} />}
+        className='w-full'
+      />
     </View>
   );
 }

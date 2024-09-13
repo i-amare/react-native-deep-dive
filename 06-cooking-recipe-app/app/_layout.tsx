0f2516431bdc8801cd24c2042d51f8e7a30d4637
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-13 10:02:09 +0200

    added drawer and favourites screen

--- 06-cooking-recipe-app/app/_layout.tsx
+++ 06-cooking-recipe-app/app/_layout.tsx
@@ -1,15 +1,31 @@
 import { FontAwesome6 } from '@expo/vector-icons';
 import { Stack, useRouter } from 'expo-router';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 export default function RootLayout() {
   return (
     <SafeAreaView className='flex-1 bg-gray-950'>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Drawer
+          screenOptions={{
+            headerTransparent: true,
+          }}
+        >
+          <Drawer.Screen name='index' options={{
+            title: 'Home'
+          }} />
+          <Drawer.Screen name='favourites/index' options={{
+            title: 'Favourites'
+          }} />
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </Drawer>
+      </GestureHandlerRootView>
     </SafeAreaView>
   );
 }

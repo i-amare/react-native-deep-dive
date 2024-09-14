41022a36c47b6ab8f96ee56150612a986df6921e
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-14 10:23:07 +0200

    removed drawer

--- 06-cooking-recipe-app/app/_layout.tsx
+++ 06-cooking-recipe-app/app/_layout.tsx
@@ -1,31 +1,16 @@
-import { FontAwesome6 } from '@expo/vector-icons';
-import { Stack, useRouter } from 'expo-router';
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Stack } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
+import { SafeAreaView } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
     <SafeAreaView className='flex-1 bg-gray-950'>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer
-          screenOptions={{
-            headerTransparent: true,
-          }}
-        >
-          <Drawer.Screen name='index' options={{
-            title: 'Home'
-          }} />
-          <Drawer.Screen name='favourites/index' options={{
-            title: 'Favourites'
-          }} />
-          <Stack
-            screenOptions={{
-              headerShown: false,
-            }}
-          />
-        </Drawer>
-      </GestureHandlerRootView>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </SafeAreaView>
   );
 }

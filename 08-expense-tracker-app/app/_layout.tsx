f43fe82d400fe92fd9372b2fbfea339f08124c9b
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-24 17:03:43 +0200

    insert gesture root view

--- 08-expense-tracker-app/app/_layout.tsx
+++ 08-expense-tracker-app/app/_layout.tsx
@@ -1,22 +1,21 @@
-import { View } from 'react-native';
 import { Slot, useRouter } from 'expo-router';
 import { NativeWindStyleSheet } from 'nativewind';
 import { useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 NativeWindStyleSheet.setOutput({
   default: 'native',
 });
 
 export default function RootLayout() {
-  
   const router = useRouter();
-  useEffect (() => {
+  useEffect(() => {
     router.navigate('/home-screen');
   }, []);
 
   return (
-    <View className='flex-1'>
+    <GestureHandlerRootView className='flex-1'>
       <Slot />
-    </View>
+    </GestureHandlerRootView>
   );
 }

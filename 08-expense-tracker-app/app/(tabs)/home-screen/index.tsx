aae1436a694afdab502475b75b2701b647d66460
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-24 19:26:22 +0200

    extracted component to separate file

--- 08-expense-tracker-app/app/(tabs)/home-screen/index.tsx
+++ 08-expense-tracker-app/app/(tabs)/home-screen/index.tsx
@@ -1,53 +1,12 @@
-import { Dimensions, Text, View } from 'react-native';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
+import BottomSheet from '@/components/home-screen/bottomSheet';
+import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View className='flex-1 bg-gray-950'>
-      <BottomSheet />
+      <BottomSheet>
+        <Text className='text-lg font-semibold text-black'>Test</Text>
+      </BottomSheet>
     </View>
-  );
-}
-
-function BottomSheet() {
-  const { height: SCREEN_HEIGHT } = Dimensions.get('window');
-  const sheetPosition = SCREEN_HEIGHT / 2.75;
-
-  const context = useSharedValue({ y: 0 });
-
-  const translateY = useSharedValue(0);
-
-  const rBottomSheet = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateY: translateY.value }],
-    };
-  });
-
-  const gesture = Gesture.Pan()
-    .onStart(() => {
-      context.value = { y: translateY.value };
-    })
-    .onUpdate((event) => {
-      const yTranslation = event.translationY + context.value.y;
-      const min = -SCREEN_HEIGHT / 2.75 + 75;
-      const max = SCREEN_HEIGHT / 7.25 - 75;
-      if (yTranslation > min && yTranslation < max) {
-        translateY.value = Math.max(yTranslation, -SCREEN_HEIGHT / 2.75 + 75);
-      }
-    });
-
-  return (
-    <GestureDetector gesture={gesture}>
-      <Animated.View
-        style={[{ top: sheetPosition }, { ...rBottomSheet }]}
-        className='absolute h-screen w-screen flex-1 rounded-t-3xl bg-white'
-      >
-        <View className='mx-auto mt-4 h-1 w-12 rounded-xl bg-gray-950'></View>
-      </Animated.View>
-    </GestureDetector>
   );
 }
---
+++ 08-expense-tracker-app/components/home-screen/bottomSheet.tsx
@@ -0,0 +1,43 @@
+import { Dimensions, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+
+export default function BottomSheet({ children }: { children?: React.ReactNode }) {
+  const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+  const sheetPosition = SCREEN_HEIGHT / 2.75;
+
+  const context = useSharedValue({ y: 0 });
+
+  const translateY = useSharedValue(0);
+
+  const rBottomSheet = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
+  });
+
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      context.value = { y: translateY.value };
+    })
+    .onUpdate((event) => {
+      const yTranslation = event.translationY + context.value.y;
+      const min = -SCREEN_HEIGHT / 2.75 + 75;
+      const max = SCREEN_HEIGHT / 7.25 - 75;
+      if (yTranslation > min && yTranslation < max) {
+        translateY.value = yTranslation;
+      }
+    });
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <Animated.View
+        style={[{ top: sheetPosition }, { ...rBottomSheet }]}
+        className='absolute h-screen w-screen flex-1 rounded-t-3xl bg-white'
+      >
+        <View className='mx-auto my-4 h-1 w-12 rounded-xl bg-gray-950' />
+        {children}
+      </Animated.View>
+    </GestureDetector>
+  );
+}

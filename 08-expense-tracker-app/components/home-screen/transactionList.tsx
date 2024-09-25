a3a7b62fc7ede3275d7cf72cec7993d06963df4c
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-25 11:48:19 +0200

    styling changes

--- 08-expense-tracker-app/components/home-screen/transactionList.tsx
+++ 08-expense-tracker-app/components/home-screen/transactionList.tsx
@@ -10,8 +10,9 @@ export default function TransactionList({ data }: TransactionListProps) {
     <FlatList
       className='mx-auto w-11/12 flex-1'
       data={data}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <TransactionItem {...item} />}
-      ListFooterComponent={<View className='h-[40vh] bg-red-400' />}
+      ListFooterComponent={<View className='h-[40vh]' />}
       ItemSeparatorComponent={() => (
         <View className='mx-auto h-1 w-full rounded-lg bg-gray-200' />
       )}
@@ -31,17 +32,17 @@ function TransactionItem({
 }: TransactionProps) {
   return (
     <View className='w-full py-2'>
-      <Text className='translate-y-1 text-sm font-semibold text-gray-500'>
+      <Text className='text-sm font-semibold text-gray-500'>
         {date.toLocaleDateString('en-US', { day: 'numeric', month: 'short' })}
       </Text>
       <View className='flex-row items-center justify-between'>
         <View>
-          <Text className='text-lg font-semibold'>{name}</Text>
-          <Text className='-translate-y-1 text-sm font-semibold text-gray-500'>
+          <Text className='font-semibold'>{name}</Text>
+          <Text className='-translate-y-1 text-sm text-gray-500'>
             {category}
           </Text>
         </View>
-        <Text className='text-lg -translate-y-1 font-semibold'>R{amount}</Text>
+        <Text className='-translate-y-1 font-semibold'>R{amount}</Text>
       </View>
     </View>
   );

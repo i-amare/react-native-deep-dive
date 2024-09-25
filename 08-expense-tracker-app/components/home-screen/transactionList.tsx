ff1f4600ffc89d1a8ca9d85483e0d459f14c6f05
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-09-25 11:35:22 +0200

    transaction item styling

--- 08-expense-tracker-app/components/home-screen/transactionList.tsx
+++ 08-expense-tracker-app/components/home-screen/transactionList.tsx
@@ -8,10 +8,13 @@ type TransactionListProps = {
 export default function TransactionList({ data }: TransactionListProps) {
   return (
     <FlatList
-      className='w-screen flex-1'
+      className='mx-auto w-11/12 flex-1'
       data={data}
       renderItem={({ item }) => <TransactionItem {...item} />}
       ListFooterComponent={<View className='h-[40vh] bg-red-400' />}
+      ItemSeparatorComponent={() => (
+        <View className='mx-auto h-1 w-full rounded-lg bg-gray-200' />
+      )}
     />
   );
 }
@@ -27,8 +30,19 @@ function TransactionItem({
   description,
 }: TransactionProps) {
   return (
-    <Text>
-      {name} - {amount} - {description} - {category} - {date.getDate()}
-    </Text>
+    <View className='w-full py-2'>
+      <Text className='translate-y-1 text-sm font-semibold text-gray-500'>
+        {date.toLocaleDateString('en-US', { day: 'numeric', month: 'short' })}
+      </Text>
+      <View className='flex-row items-center justify-between'>
+        <View>
+          <Text className='text-lg font-semibold'>{name}</Text>
+          <Text className='-translate-y-1 text-sm font-semibold text-gray-500'>
+            {category}
+          </Text>
+        </View>
+        <Text className='text-lg -translate-y-1 font-semibold'>R{amount}</Text>
+      </View>
+    </View>
   );
 }

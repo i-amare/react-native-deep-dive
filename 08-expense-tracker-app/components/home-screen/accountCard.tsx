0c203f910f52cb9121151ddc8cbe86515a17ff33
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-10-07 21:12:53 +0200

    styling update

--- 08-expense-tracker-app/components/home-screen/accountCard.tsx
+++ 08-expense-tracker-app/components/home-screen/accountCard.tsx
@@ -1,4 +1,4 @@
-import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+import { AntDesign, Feather, MaterialIcons } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
 import { Pressable, Text, View } from 'react-native';
 
@@ -8,20 +8,22 @@ interface AccountCardProps {
 
 export default function AccountCard({ balance }: AccountCardProps) {
   return (
-    <View className='mt-[10vh] w-full'>
-      <View className='flex aspect-[4/2] w-full rounded-xl bg-black p-2'>
+    <View className='w-full'>
+      <View className='flex aspect-[4/2] w-full rounded-3xl bg-black px-4 pt-6'>
         <View className='flex flex-row'>
-          <BlurView
-            intensity={10}
-            tint='extraLight'
-            className='mr-4 flex aspect-square h-12 items-center justify-center rounded-full'
-          >
-            <MaterialIcons
-              name='account-balance-wallet'
-              size={26}
-              color='white'
-            />
-          </BlurView>
+          <View className='mr-4 overflow-hidden rounded-full'>
+            <BlurView
+              intensity={10}
+              tint='extraLight'
+              className='flex aspect-square h-12 items-center justify-center rounded-full'
+            >
+              <MaterialIcons
+                name='account-balance-wallet'
+                size={26}
+                color='white'
+              />
+            </BlurView>
+          </View>
           <View className='flex'>
             <Text className='text-4xl font-semibold text-white'>
               R{balance.toFixed(2)}
@@ -30,9 +32,17 @@ export default function AccountCard({ balance }: AccountCardProps) {
           </View>
         </View>
         <View className='mt-3 flex flex-row'>
-          <Button text='Top Up' onPress={() => null} />
+          <Button
+            text='Top Up'
+            onPress={() => null}
+            icon={<AntDesign name='plus' color='white' size={12} />}
+          />
           <View className='mr-2'></View>
-          <Button text='Send Money' onPress={() => null} />
+          <Button
+            text='Send Money'
+            onPress={() => null}
+            icon={<Feather name='arrow-up-right' color='white' size={12} />}
+          />
         </View>
       </View>
     </View>
@@ -42,15 +52,16 @@ export default function AccountCard({ balance }: AccountCardProps) {
 interface ButtonProps {
   onPress: () => void;
   text: string;
+  icon: React.ReactNode;
 }
 
-function Button({ text, onPress }: ButtonProps) {
+function Button({ text, icon, onPress }: ButtonProps) {
   return (
     <Pressable
       onPress={onPress}
       className={`flex-row items-center rounded-2xl bg-gray-800 px-4 py-2`}
     >
-      <AntDesign name='plus' color='white' size={12} />
+      {icon}
       <View className='mr-2' />
       <Text className='-translate-y-[1px] text-center text-xs text-white'>
         {text}

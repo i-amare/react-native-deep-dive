9e06b14c518a1f58322e5bc5e19c8849e3662f8a
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-10-07 20:52:16 +0200

    update scroll animation

--- 08-expense-tracker-app/app/(tabs)/home-screen/index.tsx
+++ 08-expense-tracker-app/app/(tabs)/home-screen/index.tsx
@@ -1,3 +1,5 @@
+import AccountCard from '@/components/home-screen/accountCard';
+import TransactionList from '@/components/home-screen/transactionList';
 import { AccountContext } from '@/context/AccountContext';
 import { useContext } from 'react';
 import { View } from 'react-native';
@@ -7,7 +9,8 @@ import Animated, {
   useSharedValue,
 } from 'react-native-reanimated';
 
-const SCROLL_RATIO = 1.15;
+const SCROLL_RATIO = 1.5;
+const CARD_TRANSLATION = 50;
 
 export default function HomeScreen() {
   const accountContext = useContext(AccountContext);
@@ -29,29 +32,20 @@ export default function HomeScreen() {
   }));
 
   const slowScroll = useAnimatedStyle(() => ({
-    transform: [{ translateY: translateY.value / SCROLL_RATIO }],
+    transform: [{ translateY: translateY.value / SCROLL_RATIO + CARD_TRANSLATION }],
   }));
 
   return (
-    <View className='flex-1 items-center bg-green-900'>
-      <Animated.View
-        style={slowScroll}
-        className='h-[25vh] w-full bg-red-400'
-      ></Animated.View>
+    <View className='flex-1 items-center bg-green-900 pt-14'>
+      <Animated.View style={slowScroll} className='w-full'>
+        <AccountCard balance={accountContext.balance} />
+      </Animated.View>
       <GestureDetector gesture={panGesture}>
-        <Animated.View
-          style={fastScroll}
-          className='mx-auto h-2 w-16 rounded-md bg-gray-400'
-        ></Animated.View>
+        <Animated.View style={fastScroll} className='w-full bg-white rounded-3xl'>
+          <View className='mx-auto my-4 h-1 w-12 rounded-xl bg-gray-950' />
+          <TransactionList data={accountContext.transactionHistory} />
+        </Animated.View>
       </GestureDetector>
-      <Animated.View
-        style={fastScroll}
-        className='h-[50vh] w-full bg-blue-400'
-      ></Animated.View>
-      {/* <AccountCard balance={accountContext.balance} />
-      <BottomSheet>
-        <TransactionList data={accountContext.transactionHistory} />
-      </BottomSheet> */}
     </View>
   );
 }

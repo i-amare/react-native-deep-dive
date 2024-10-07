a22432a182a985be3156ae91d1fb9bb918d31e34
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-10-07 21:12:40 +0200

    update scroll animation

--- 08-expense-tracker-app/app/(tabs)/home-screen/index.tsx
+++ 08-expense-tracker-app/app/(tabs)/home-screen/index.tsx
@@ -2,7 +2,7 @@ import AccountCard from '@/components/home-screen/accountCard';
 import TransactionList from '@/components/home-screen/transactionList';
 import { AccountContext } from '@/context/AccountContext';
 import { useContext } from 'react';
-import { View } from 'react-native';
+import { Dimensions, SafeAreaView, View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedStyle,
@@ -15,6 +15,10 @@ const CARD_TRANSLATION = 50;
 export default function HomeScreen() {
   const accountContext = useContext(AccountContext);
 
+  const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+  const MAX_SCROLL = 75;
+  const MIN_SCROLL = -125;
+
   const scrollContext = useSharedValue(0);
   const translateY = useSharedValue(0);
 
@@ -23,7 +27,9 @@ export default function HomeScreen() {
       scrollContext.value = translateY.value;
     })
     .onUpdate((e) => {
-      translateY.value = e.translationY + scrollContext.value;
+      const newTranslation = e.translationY + scrollContext.value;
+      if (newTranslation > MIN_SCROLL && newTranslation < MAX_SCROLL)
+        translateY.value = newTranslation;
       console.log(translateY.value);
     });
 
@@ -32,20 +38,25 @@ export default function HomeScreen() {
   }));
 
   const slowScroll = useAnimatedStyle(() => ({
-    transform: [{ translateY: translateY.value / SCROLL_RATIO + CARD_TRANSLATION }],
+    transform: [
+      { translateY: translateY.value / SCROLL_RATIO + CARD_TRANSLATION },
+    ],
   }));
 
   return (
-    <View className='flex-1 items-center bg-green-900 pt-14'>
+    <SafeAreaView className='flex-1 items-center bg-green-900 pt-14'>
       <Animated.View style={slowScroll} className='w-full'>
         <AccountCard balance={accountContext.balance} />
       </Animated.View>
       <GestureDetector gesture={panGesture}>
-        <Animated.View style={fastScroll} className='w-full bg-white rounded-3xl'>
+        <Animated.View
+          style={fastScroll}
+          className='h-screen w-full rounded-3xl bg-white'
+        >
           <View className='mx-auto my-4 h-1 w-12 rounded-xl bg-gray-950' />
           <TransactionList data={accountContext.transactionHistory} />
         </Animated.View>
       </GestureDetector>
-    </View>
+    </SafeAreaView>
   );
 }

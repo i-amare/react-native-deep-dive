066763dd3c2fa153c5caae13f954681a2123892a
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-10-20 23:21:52 +0200

    centred header title

--- 08-expense-tracker-app/components/ui/pageHeader.tsx
+++ 08-expense-tracker-app/components/ui/pageHeader.tsx
@@ -1,7 +1,7 @@
 import { AntDesign } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
 import { useRouter } from 'expo-router';
-import { Pressable, SafeAreaView, Text } from 'react-native';
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
 
 interface PageHeaderProps {
   title: string;
@@ -10,7 +10,12 @@ export default function PageHeader({ title }: PageHeaderProps) {
   const router = useRouter();
 
   return (
-    <SafeAreaView className='relative flex w-full flex-row items-center justify-start p-4'>
+    <SafeAreaView className='relative left-0 top-0 flex w-full flex-row items-center justify-start'>
+      <View className='absolute bottom-0 left-0 -z-10 flex h-full w-full items-center justify-end'>
+        <Text className='text-center text-lg font-semibold text-white'>
+          {title}
+        </Text>
+      </View>
       <Pressable
         onPress={() => router.back()}
         className='overflow-hidden rounded-full'
@@ -19,9 +24,6 @@ export default function PageHeader({ title }: PageHeaderProps) {
           <AntDesign size={22} color='white' name='arrowleft' />
         </BlurView>
       </Pressable>
-      <Text className='text-center m-auto text-lg font-semibold text-white'>
-        {title}
-      </Text>
     </SafeAreaView>
   );
 }

8059a3b60916005964464709739cee375c2ae220
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-10-21 21:53:22 +0200

    fixed positioning errors

--- 08-expense-tracker-app/components/ui/pageHeader.tsx
+++ 08-expense-tracker-app/components/ui/pageHeader.tsx
@@ -1,29 +1,34 @@
 import { AntDesign } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
 import { useRouter } from 'expo-router';
-import { Pressable, SafeAreaView, Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface PageHeaderProps {
   title: string;
 }
 export default function PageHeader({ title }: PageHeaderProps) {
   const router = useRouter();
+  const safeArea = useSafeAreaInsets();
 
   return (
-    <SafeAreaView className='relative left-0 top-0 flex w-full flex-row items-center justify-start'>
-      <View className='absolute bottom-0 left-0 -z-10 flex h-full w-full items-center justify-end'>
+    <View
+      style={{ transform: `translateY(${safeArea.top}px)` }}
+      className='mt-4 flex w-full items-start justify-center'
+    >
+      <View className='absolute flex h-full w-full justify-center'>
         <Text className='text-center text-lg font-semibold text-white'>
           {title}
         </Text>
       </View>
       <Pressable
         onPress={() => router.back()}
-        className='overflow-hidden rounded-full'
+        className='ml-4 overflow-hidden rounded-full'
       >
         <BlurView className='rounded-full p-2'>
           <AntDesign size={22} color='white' name='arrowleft' />
         </BlurView>
       </Pressable>
-    </SafeAreaView>
+    </View>
   );
 }

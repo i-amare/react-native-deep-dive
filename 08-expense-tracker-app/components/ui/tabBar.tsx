c53ba354da79e9c90ca2e27fd4c2a694a39d15ef
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-11-21 21:19:44 +0200

    added ts-ignore

--- 08-expense-tracker-app/components/ui/tabBar.tsx
+++ 08-expense-tracker-app/components/ui/tabBar.tsx
@@ -1,4 +1,5 @@
 import { FontAwesome } from '@expo/vector-icons';
+// @ts-ignore
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { Pressable, View } from 'react-native';
 
@@ -9,6 +10,7 @@ type TabIconProps = {
 };
 
 const TabIcon = ({ name, color, size }: TabIconProps) => (
+  // @ts-ignore
   <FontAwesome name={name} color={color} size={size} />
 );
 
@@ -22,22 +24,29 @@ const getIconName = (routeName: string): string => {
 
 export default function TabBar({ state, navigation }: BottomTabBarProps) {
   return (
-    <View className='absolute bottom-0 left-0 flex w-full flex-row items-start justify-start'>
-      <View className='mx-auto mb-12 flex flex-row justify-evenly rounded-3xl bg-gray-950 py-4'>
-        {state.routes.map((route, index) => {
-          const isActive = state.index === index;
-          const color = isActive ? 'white' : 'gray';
+    <View className='pointer-events-none absolute bottom-0 left-0 right-0 mx-auto mb-12 items-center justify-end'>
+      <View className='mx-auto flex flex-row justify-evenly rounded-3xl bg-gray-950 py-4'>
+        {
+          // @ts-ignore
+          state.routes.map((route, index) => {
+            const isActive = state.index === index;
+            const color = isActive ? 'white' : 'gray';
 
-          return (
-            <Pressable
-              key={route.key}
-              className='mx-6 flex items-center justify-center transition-all duration-700 active:scale-90 active:opacity-75'
-              onPress={() => navigation.navigate(route.name)}
-            >
-              <TabIcon name={getIconName(route.name)} color={color} size={24} />
-            </Pressable>
-          );
-        })}
+            return (
+              <Pressable
+                key={route.key}
+                className='mx-6 flex items-center justify-center transition-all duration-700 active:scale-90 active:opacity-75'
+                onPress={() => navigation.navigate(route.name)}
+              >
+                <TabIcon
+                  name={getIconName(route.name)}
+                  color={color}
+                  size={24}
+                />
+              </Pressable>
+            );
+          })
+        }
       </View>
     </View>
   );

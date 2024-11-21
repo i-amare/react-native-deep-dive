c4aa1e7cb29d0df8e68894ab09a753b4f6b9213d
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-11-21 18:24:37 +0200

    refactor

--- 08-expense-tracker-app/components/ui/numericKeyboard.tsx
+++ 08-expense-tracker-app/components/ui/numericKeyboard.tsx
@@ -6,70 +6,57 @@ interface NumericKeyboardProps {
   onBackSpace: () => void;
 }
 
+const keyValues = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  ',',
+  '0',
+  'backspace',
+];
+
 export default function NumericKeyboard({
   onKeyPress,
   onBackSpace,
 }: NumericKeyboardProps) {
-  const keyValues = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    ',',
-    '0',
-    'backspace',
-  ];
-
   return (
     <View className='absolute bottom-0 left-0 h-[40vh] w-full border-t-2 border-t-gray-800'>
       <View className='flex h-full flex-row flex-wrap'>
-        {keyValues.map((value, idx) => (
-          <RenderKeyboardKey
-            value={value}
-            onBackSpace={onBackSpace}
-            onKeyPress={onKeyPress}
-            idx={idx}
-          />
-        ))}
+        {keyValues.map((value, idx) =>
+          renderKey(idx, value, onBackSpace, onKeyPress),
+        )}
       </View>
     </View>
   );
 }
 
-interface KeyProps {
-  onKeyPress: (value: string) => void;
-  onBackSpace: () => void;
-  value: string;
-  idx: number;
-}
-
-function RenderKeyboardKey({ value, onKeyPress, onBackSpace, idx }: KeyProps) {
-  const content = (() => {
-    if (value === 'backspace') {
-      return <FontAwesome5 name='backspace' size={24} color='white' />;
-    } else if (value !== null) {
-      return <Text className='text-xl font-semibold text-white'>{value}</Text>;
-    } else {
-      return null;
-    }
-  })();
-
+function renderKey(
+  idx: number,
+  value: string,
+  onBackSpace: () => void,
+  onKeyPress: (value: string) => void,
+) {
   return (
     <TouchableHighlight
       key={idx}
       className='h-1/4 w-1/3'
       onPress={() =>
-        value ? onKeyPress(value) : value === null ? null : onBackSpace()
+        value === 'backspace' ? onBackSpace() : onKeyPress(value)
       }
       underlayColor='rgba(255, 255, 255, 0.2)'
     >
       <View className='h-full w-full items-center justify-center'>
-        {content}
+        {value === 'backspace' ? (
+          <FontAwesome5 name='backspace' size={24} color='white' />
+        ) : (
+          <Text className='text-xl font-semibold text-white'>{value}</Text>
+        )}
       </View>
     </TouchableHighlight>
   );

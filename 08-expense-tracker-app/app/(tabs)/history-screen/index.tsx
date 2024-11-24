4862ddf913bc8a0550d0d0b9789d194e381857a0
Amaré Ngobeni <n.ngobeni@icloud.com>
2024-11-24 13:05:20 +0200

    added transaction history panel

--- 08-expense-tracker-app/app/(tabs)/history-screen/index.tsx
+++ 08-expense-tracker-app/app/(tabs)/history-screen/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import TransactionList from '@/components/transactionList';
+import { AccountContext } from '@/context/AccountContext';
+import { BlurView } from 'expo-blur';
+import { useContext } from 'react';
+import { Image, SafeAreaView, Text, View } from 'react-native';
 
 export default function HistoryScreen() {
+  const { transactionHistory } = useContext(AccountContext);
+
   return (
-    <View className='flex-1 bg-blue-400'>
-      <Text>Folder History Screen</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-gray-950'>
+      <Image
+        source={require('@/assets/images/bg-image.jpg')}
+        resizeMode='stretch'
+        className='absolute left-0 top-0 h-screen w-full'
+      />
+      <Text className='m-4 mb-8 text-3xl font-bold text-white'>
+        Transaction History
+      </Text>
+      <BlurView
+        className='h-full w-full rounded-3xl pt-8'
+        intensity={80}
+        tint='dark'
+      >
+        <TransactionList data={transactionHistory} />
+      </BlurView>
+    </SafeAreaView>
   );
 }

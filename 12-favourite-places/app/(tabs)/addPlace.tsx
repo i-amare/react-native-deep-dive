63517085ac121f2c89d5dec52dfc68872f48cb33
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-03 12:41:41 +0200

    Revert "setting up project layout"
    This reverts commit c659708a7ecbf335ed2f915ab058a58dd7b1a528.

---
+++ 12-favourite-places/app/(tabs)/addPlace.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function AddPlace() {
+	return (
+		<View className='flex-1 justify-center items-center bg-black'>
+			<Text className='text-3xl text-white'>Add Place</Text>
+		</View>
+	);
+}
--- 12-favourite-places/app/(tabs)/index.tsx
+++ 12-favourite-places/app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
-	return <View className='flex-1 bg-black'></View>;
+	return (
+		<View className='flex-1 bg-black items-center justify-center'>
+			<Text className='text-3xl text-white'>All Places</Text>
+		</View>
+	);
 }

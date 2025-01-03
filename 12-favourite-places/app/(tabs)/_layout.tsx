966a557af81e64dc119886fb783fb0bb8b367f3b
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-03 11:54:29 +0200

    project setup

---
+++ 12-favourite-places/app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+	return (
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+				tabBarStyle: Platform.select({
+					ios: {
+						// Use a transparent background on iOS to show the blur effect
+						position: "absolute",
+					},
+					default: {},
+				}),
+			}}
+		>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: "Home",
+					tabBarIcon: ({ color }) => (
+						<MaterialIcons size={28} name='house' color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
+}
---
+++ 12-favourite-places/app/+not-found.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NotFoundScreen() {
+	return (
+		<View className='flex-1 items-center justify-center'>
+			<Text className='text-xl'>404 Not Found</Text>
+		</View>
+	);
+}

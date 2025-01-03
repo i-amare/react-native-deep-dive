cafe9d6414d9d703bb6a6d5d8d6322442a126ef8
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-03 13:12:37 +0200

    create storage util function

---
+++ 12-favourite-places/utils/store.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const storeData = async ({ key, value }: { key: string; value: any }) => {
+	try {
+		await AsyncStorage.setItem(key, value);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+const readDate = async (key: string) => {
+	try {
+		return await AsyncStorage.getItem(key);
+	} catch (e) {
+		console.error(e);
+	}
+};

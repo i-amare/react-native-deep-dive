1b271c04f371b756cd896ea5b4e78382c5d1b8cd
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-03 13:22:49 +0200

    added module exports

--- 12-favourite-places/utils/store.ts
+++ 12-favourite-places/utils/store.ts
@@ -1,6 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const storeData = async ({ key, value }: { key: string; value: any }) => {
+type Stores = "PlacesList";
+
+export const storeData = async ({
+	key,
+	value,
+}: {
+	key: string;
+	value: any;
+}) => {
 	try {
 		await AsyncStorage.setItem(key, value);
 	} catch (e) {
@@ -8,7 +16,7 @@ const storeData = async ({ key, value }: { key: string; value: any }) => {
 	}
 };
 
-const readData = async (key: string) => {
+export const readData = async (key: Stores) => {
 	try {
 		return await AsyncStorage.getItem(key);
 	} catch (e) {

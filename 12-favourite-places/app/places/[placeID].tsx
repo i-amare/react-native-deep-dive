5070c96f6402e0b7f93075303044a3025bfc9da1
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-04 10:01:59 +0200

    render dynamic place routes

--- 12-favourite-places/app/places/[placeID].tsx
+++ 12-favourite-places/app/places/[placeID].tsx
@@ -1,5 +1,25 @@
-import { View } from "react-native";
+import { PlacesContext } from "@/contexts/PlacesContext";
+import { useLocalSearchParams } from "expo-router";
+import { useContext } from "react";
+import { Image, Text, View } from "react-native";
 
 export default function Place() {
-	return <View className='flex-1 bg-black'></View>;
+  const { places } = useContext(PlacesContext);
+  const placeID = useLocalSearchParams();
+
+  return (
+    <View className='my-2 aspect-square w-full rounded-lg bg-gray-900'>
+      <Image
+        source={{
+          uri: "https://www.secretatlas.com/wp-content/uploads/2021/03/Hamnoy_Lofoten_Islands_Norway_Shutterstock_SecretAtlas_WebVersion.jpg",
+        }}
+        className='h-2/3 w-full rounded-t-lg'
+      />
+      <View className='px-4 py-2'>
+        <Text className='text-xl text-white'>{JSON.stringify(placeID)}</Text>
+        {/* <Text className='text-xl text-white'>{place.title}</Text>
+        <Text className='text-lg text-white'>{place.address}</Text> */}
+      </View>
+    </View>
+  );
 }

fab49b7f5a6bce36bdc5fa85c017293e74f492d3
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-04 11:36:42 +0200

    small changes

--- 12-favourite-places/app/(tabs)/addPlace.tsx
+++ 12-favourite-places/app/(tabs)/addPlace.tsx
@@ -2,7 +2,9 @@ import Place from "@/classes/Place";
 import { PlacesContext } from "@/contexts/PlacesContext";
 import { useRouter } from "expo-router";
 import { useContext, useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FormInput } from "../../components/formInput";
 
 export default function AddPlace() {
   const { addPlace } = useContext(PlacesContext);
@@ -14,57 +16,36 @@ export default function AddPlace() {
   const onAddPlaceButton = () => {
     const place = new Place(placeName, placeAddress, { lat: 0, lng: 0 }, "");
     addPlace(place);
+
+    setPlaceName("");
+    setPlaceAddress("");
+
     router.navigate("/(tabs)");
   };
 
   return (
-    <View className='flex-1 items-center justify-start bg-black p-8'>
-      <Text className='text-3xl text-white'>Add Place</Text>
-      <FormInput
-        label='Place Name'
-        value={placeName}
-        placeholder='Enter Place Name'
-        onChange={(value) => setPlaceName(value)}
-      />
-      <FormInput
-        label='Address'
-        value={placeAddress}
-        placeholder='Enter Address'
-        onChange={(value) => setPlaceAddress(value)}
-      />
-      <Pressable
-        onPressIn={onAddPlaceButton}
-        className='mt-6 flex w-full items-center justify-center rounded-lg bg-gray-900 py-4'
-      >
-        <Text className='text-xl font-semibold text-white'>Add Place</Text>
-      </Pressable>
-    </View>
-  );
-}
-
-interface FormInputProps {
-  label: string;
-  value: string;
-  placeholder: string;
-  onChange: (value: string) => void;
-}
-
-export function FormInput({
-  label,
-  value,
-  placeholder,
-  onChange,
-}: FormInputProps) {
-  return (
-    <View className='my-2 w-full'>
-      <Text className='mb-1 text-lg font-semibold text-white'>{label}</Text>
-      <TextInput
-        value={value}
-        placeholder={placeholder}
-        placeholderTextColor='#ccc'
-        onChangeText={(text) => onChange(text)}
-        className='w-full rounded-md bg-white px-2 py-3 text-lg text-black'
-      />
-    </View>
+    <SafeAreaView className='flex-1'>
+      <View className='flex-1 items-center justify-start bg-black p-8'>
+        <Text className='text-3xl text-white'>Add Place</Text>
+        <FormInput
+          label='Place Name'
+          value={placeName}
+          placeholder='Enter Place Name'
+          onChange={(value) => setPlaceName(value)}
+        />
+        <FormInput
+          label='Address'
+          value={placeAddress}
+          placeholder='Enter Address'
+          onChange={(value) => setPlaceAddress(value)}
+        />
+        <Pressable
+          onPressIn={onAddPlaceButton}
+          className='mt-6 flex w-full items-center justify-center rounded-lg bg-gray-900 py-4'
+        >
+          <Text className='text-xl font-semibold text-white'>Add Place</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
--- 12-favourite-places/app/(tabs)/index.tsx
+++ 12-favourite-places/app/(tabs)/index.tsx
@@ -1,19 +1,21 @@
 import PlaceItem from "@/components/placeItem";
 import { PlacesContext } from "@/contexts/PlacesContext";
 import { useContext } from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
 
 export default function HomeScreen() {
   const { places, addPlace, removePlace } = useContext(PlacesContext);
 
   return (
-    <View className='flex-1 items-center justify-center bg-black p-8 font-semibold'>
-      <Text className='text-3xl text-white'>All Places</Text>
-      <FlatList
-        data={places}
-        renderItem={({ item }) => <PlaceItem place={item} />}
-        className='w-full'
-      />
-    </View>
+    <SafeAreaView className='flex-1'>
+      <View className='flex-1 items-center justify-center bg-black p-8 font-semibold'>
+        <Text className='text-3xl text-white'>All Places</Text>
+        <FlatList
+          data={places}
+          renderItem={({ item }) => <PlaceItem place={item} />}
+          className='w-full'
+        />
+      </View>
+    </SafeAreaView>
   );
 }

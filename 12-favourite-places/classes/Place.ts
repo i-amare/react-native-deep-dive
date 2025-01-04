00d0197f8a9b70349bc3dd99b9b40fa115e8ab5e
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-04 10:02:36 +0200

    update ID generation

--- 12-favourite-places/classes/Place.ts
+++ 12-favourite-places/classes/Place.ts
@@ -14,7 +14,7 @@ export default class Place {
     location: { lat: number; lng: number },
     imageURL: string,
   ) {
-    this.placeID = new Date().toString() + Math.random().toString();
+    this.placeID = `${new Date().getTime().toString(16)}-${Math.random().toString(16)}`;
     this.title = title;
     this.address = address;
     this.location = location;

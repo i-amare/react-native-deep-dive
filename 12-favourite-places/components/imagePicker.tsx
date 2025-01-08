53f7f653c8d24cf78c80737d824fab9f6a571b98
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-08 12:27:56 +0200

    Additionaling styling and layout changes

--- 12-favourite-places/components/imagePicker.tsx
+++ 12-favourite-places/components/imagePicker.tsx
@@ -1,6 +1,7 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import * as ImagePickerAPI from "expo-image-picker";
 import { useEffect, useState } from "react";
-import { Image, Pressable, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 interface ImagePickerProps {
   setPlaceImage: (image: string) => void;
@@ -33,11 +34,12 @@ export default function ImagePicker({ setPlaceImage }: ImagePickerProps) {
     <View className='mt-6 w-full'>
       <Pressable
         onPressIn={() => pickImage()}
-        className='flex items-center justify-center rounded-md bg-gray-900 p-4'
+        className='flex flex-row items-center justify-center rounded-md bg-gray-900 p-4'
       >
-        <Text className='text-xl font-semibold text-white'>
-          Choose Image from Camera Roll
+        <Text className='mr-4 text-xl font-semibold text-white'>
+          Choose Image
         </Text>
+        <MaterialIcons name='add-a-photo' size={20} color='white' />
       </Pressable>
     </View>
   );

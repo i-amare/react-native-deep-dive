7ba4a83fc0e130da9faf81981141795618d63e0f
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-01-10 12:00:55 +0200

    Added ability to delete a place

--- 12-favourite-places/app/places/[placeID].tsx
+++ 12-favourite-places/app/places/[placeID].tsx
@@ -1,35 +1,59 @@
 import { PlacesContext } from "@/contexts/PlacesContext";
-import { Feather } from "@expo/vector-icons";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Feather, MaterialIcons } from "@expo/vector-icons";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import { useContext } from "react";
 import { Image, Pressable, Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 export default function Place() {
-  const { places } = useContext(PlacesContext);
+  const { places, removePlace } = useContext(PlacesContext);
   const placeID = useLocalSearchParams().placeID as string;
   const place = places.find((place) => place.placeID === placeID);
+  const router = useRouter();
+
+  const onDeleteButton = () => {
+    console.log("Delete button pressed");
+    removePlace(placeID);
+    router.navigate("/");
+  };
 
   return (
-    <View className='my-2 aspect-square w-full rounded-lg bg-gray-900'>
-      <Stack.Screen
-        options={{
-          header: ({ navigation }) => (
-            <ScreenHeader title={place?.title || ""} navigation={navigation} />
-          ),
-        }}
-      />
-      <Image
-        source={{
-          uri: place?.imageURL,
-        }}
-        className='h-2/3 w-full rounded-t-lg'
-      />
-      <View className='px-4 py-2'>
-        <Text className='text-xl text-white'>{JSON.stringify(placeID)}</Text>
-        <Text className='text-xl text-white'>{place?.title}</Text>
+    <SafeAreaView className='flex flex-1 items-center'>
+      <View className='my-2 aspect-square w-full rounded-lg bg-gray-900'>
+        <Stack.Screen
+          options={{
+            header: ({ navigation }) => (
+              <ScreenHeader
+                title={place?.title || ""}
+                navigation={navigation}
+              />
+            ),
+          }}
+        />
+        <Image
+          source={{
+            uri: place?.imageURL,
+          }}
+          className='h-2/3 w-full rounded-t-lg'
+        />
+        <View className='px-4 py-2'>
+          <Text className='text-xl text-white'>{JSON.stringify(placeID)}</Text>
+          <Text className='text-xl text-white'>{place?.title}</Text>
+        </View>
       </View>
-    </View>
+      <View className='w-full p-4'>
+        <Pressable
+          onPressIn={onDeleteButton}
+          className='flex w-full flex-row items-center justify-center gap-4 rounded-lg bg-red-800 py-4'
+        >
+          <Text className='text-xl text-white'>Delete</Text>
+          <MaterialIcons name='delete' size={24} color='white' />
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
